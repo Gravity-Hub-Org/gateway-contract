74266b9a3c2fab2bb86d30af5cff585b31711aaf
CryptoBadBoy <raiman.elshan@gmail.com>
2020-07-15 17:33:49 +0300

    fix

--- deployer/models/models.go
+++ deployer/models/models.go
@@ -1,12 +1,29 @@
 package models
 
 type States []State
+type Status int
+type RqType int
 
 type State struct {
 	Key   string      `json:"key"`
 	Value interface{} `json:"value"`
 	Type  string      `json:"type"`
 }
+
+const (
+	None Status = iota
+	New
+	Rejected
+	Success
+	Returned
+)
+
+const (
+	Lock RqType = iota
+	Unlock
+	Mint
+	Burn
+)
 
 func (states States) Map() map[string]State {
 	stateMap := make(map[string]State)
