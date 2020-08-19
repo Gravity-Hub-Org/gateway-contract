9757a717f56adb9b761c5a261f0edffbf5722fe3
CryptoBadBoy <raiman.elshan@gmail.com>
2020-08-19 10:48:08 +0300

    refactoring

--- test/ethereum/go/helpers/config.go
+++ test/ethereum/go/helpers/config.go
@@ -1,41 +1,47 @@
 package helpers
 
 import (
-    "log"
-	"os"
 	"encoding/json"
+	"os"
 )
 
 type Config struct {
-    Endpoint string `json:"endpoint"`
-    OraclePK [5]string `json:"oraclepk"`
+	Endpoint string    `json:"endpoint"`
+	OraclePK [5]string `json:"oraclepk"`
 }
 
 type NebulaData struct {
-    Abi string
-    Address string
+	Abi     string
+	Address string
 }
 
-
-func LoadConfiguration() (Config, NebulaData) {
+func LoadConfiguration() (Config, NebulaData, error) {
 	var config Config
 	var nebulaData NebulaData
 
 	configFile, err := os.Open("config.json")
-    defer configFile.Close()
-    if err != nil {
-        log.Fatal(err)
-    }
-    jsonParser := json.NewDecoder(configFile)
-    jsonParser.Decode(&config)
+	defer configFile.Close()
 
-    nebulaDataFile, err := os.Open("nebula.json")
-    defer nebulaDataFile.Close()
-    if err != nil {
-        log.Fatal(err)
-    }
-    jsonParser2 := json.NewDecoder(nebulaDataFile)
-	jsonParser2.Decode(&nebulaData)
-	return config, nebulaData
+	if err != nil {
+		return Config{}, NebulaData{}, err
+	}
+
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&config)
+	if err != nil {
+		return Config{}, NebulaData{}, err
+	}
+
+	nebulaDataFile, err := os.Open("nebula.json")
+	defer nebulaDataFile.Close()
+
+	if err != nil {
+		return Config{}, NebulaData{}, err
+	}
+	jsonParser2 := json.NewDecoder(nebulaDataFile)
+	err = jsonParser2.Decode(&nebulaData)
+	if err != nil {
+		return Config{}, NebulaData{}, err
+	}
+	return config, nebulaData, nil
 }
-
--- test/ethereum/go/nebula_pulse_test.go
+++ test/ethereum/go/nebula_pulse_test.go
@@ -1,60 +1,70 @@
 package nebula_test
 
 import (
-	"nebula-test/helpers"
-	"bytes"
-	"os"
-	"log"
-	"testing"
+	"context"
 	"math/rand"
+	"nebula-test/helpers"
+	"os"
+	"testing"
+
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+var ctx context.Context
 var ethConnection *ethclient.Client
 var nebulaContract helpers.NebulaCaller
 var config helpers.Config
 var nebulaData helpers.NebulaData
 
 func TestMain(m *testing.M) {
-    config, nebulaData = helpers.LoadConfiguration()
-    ethConnection = helpers.ConnectClient(config.Endpoint)
-    nebulaContract = helpers.NewNebula(nebulaData.Address, nebulaData.Abi, ethConnection, config.OraclePK[:])
+	var err error
+	config, nebulaData, err = helpers.LoadConfiguration()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx = context.Background()
+	ethConnection, err = ethclient.DialContext(ctx, config.Endpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	nebulaContract = helpers.NewNebula(nebulaData.Address, nebulaData.Abi, ethConnection, config.OraclePK[:])
 	os.Exit(m.Run())
 }
 
-func Random32Byte() ([32]byte) {
-    var out [32]byte
-    rand.Read(out[:])
-    return out
+func Random32Byte() [32]byte {
+	var out [32]byte
+	rand.Read(out[:])
+	return out
 }
 
 func TestPulseSaved(t *testing.T) {
-    d := Random32Byte()
-    if !nebulaContract.SignData(d, 5) {
-		log.Fatal("can't send signed data")
-        t.Fail()
-    }
-    height := helpers.GetCurrentHeight(ethConnection)
-    pulseData := nebulaContract.GetPulseHash(height)
+	d := Random32Byte()
+	if !nebulaContract.SignData(d, 5) {
+		t.Error("can't send signed data")
+	}
+	height, err := ethConnection.BlockByNumber(ctx, nil)
+	if err != nil {
+		t.Error(err)
+	}
+	pulseData := nebulaContract.GetPulseHash(height.Number())
 
-    if bytes.Compare(d[:], pulseData[:]) != 0 {
-		log.Fatal("data mismatch")
-        t.Fail()
-    }
+	if d != pulseData {
+		t.Error("data mismatch")
+	}
 }
 
-func TestPulseCorect3(t *testing.T) {
-    d := Random32Byte()
-    if !nebulaContract.SignData(d, 3) {
-		log.Fatal("can't send signed data")
-        t.Fail()
-    }
+func TestPulseCorrect3(t *testing.T) {
+	d := Random32Byte()
+	if !nebulaContract.SignData(d, 3) {
+		t.Error("can't send signed data")
+	}
 }
 
-func TestPulseInCorect2(t *testing.T) {
-    d := Random32Byte()
-    if nebulaContract.SignData(d, 2) {
-		log.Fatal("transaction 2/3 valid sigs should be rejected")
-        t.Fail()
-    }
+func TestPulseInCorrect2(t *testing.T) {
+	d := Random32Byte()
+	if nebulaContract.SignData(d, 2) {
+		t.Error("transaction 2/3 valid sigs should be rejected")
+	}
 }
